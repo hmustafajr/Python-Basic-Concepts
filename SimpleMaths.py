26f50e4eb68d46c87b537f718addf85fe8b6c32b
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-03-04 16:26:15 -0800

    Update SimpleMaths.py

--- SimpleMaths.py
+++ SimpleMaths.py
@@ -1,6 +1,10 @@
+# Python has the capability of carrying out calculations.
+
 1 + 1
 # Outputs '2'
-# There is no need to declare variables, simply submit the algorithm observe results
 
 5 + 4 - 3
 # Outputs '6'
+
+# Enter an algorithm directly into the console and it will output the answer
+# magic
