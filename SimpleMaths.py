5558861d03d4fa70c99a5309837f04f05758d0f3
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-03-06 13:57:07 -0800

    Update SimpleMaths.py

--- SimpleMaths.py
+++ SimpleMaths.py
@@ -1,28 +1,28 @@
 # Python has the capability of carrying out calculations.
-
 1 + 1 #2
-
 5 + 4 - 3 #6
 
 # Enter an algorithm directly into the console and it will output the answer
 
-
-#Python will carry out multiplication and division as well by using the standard operators
-
+# Python will carry out multiplication and division as well by using the standard operators
 2 * (3 + 4) # 14
-
 10 /2 #5.0
+
 # using a single slash for some reason creates a Float
 
 # If you are manipiulating negative values you will simply use the '-' sign
 - 5
-
 (-5 + 2) * (-3) #9
 
-# magic
+# Yoy can add the '+' sign as well but it only serves to notify the reader it is a positive value
++7
 
 # My QA brain likes to break things, what happens when you divide by 0?
 11 / 0
+
 # Traceback (most recent call last):
 #  File "<stdin> line 25, in <module>
 # Zero DivisionError: division by zero
+
+
+# magic
