863cdf7dd33643bb0e8b564f06fd68777dbc06db
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-03-06 14:34:46 -0800

    Update Quotient & Remainder.py

--- AdvancedMaths/Quotient & Remainder.py
+++ AdvancedMaths/Quotient & Remainder.py
@@ -1,1 +1,17 @@
+# To determine the quotient and remainder of a division you will use the floor division and modulo operators, respectively
 
+# Floor division is done using double forward slashes, which explains why comments are done differently
+>>> 20 // 6
+3
+
+# Modulo operator is carried out with the '%' symbol
+>>> 1.25 % 0.5
+0.25
+
+# These operators can be used with both floats and integers
+
+# One noticed was that
+>>> 20 % 6
+# returns
+2
+# because 3 * 6 + 2 = 20
