a6817dc7a5d324caa223eb089ee46ffeb30b8288
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-03-06 14:07:15 -0800

    Create Floats.py

---
+++ Floats.py
@@ -0,0 +1,14 @@
+# Floats are used to represent numbers that are not integers
+# They can be created directly by entering a number with a decimal point or by using division
+3 / 4 # 0.75
+9.8765 # 9.8765 is returned and stored as a float
+
+# Dividing any two integers will produce a float
+# A float is also produced by running an operation on two floats or on a float and an integer
+
+>>> 8 / 2
+4.0
+>>> 6 * 7.0
+42.0
+>>> 4 + 1.65
+5.65
