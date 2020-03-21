7427c92f021637f27c753e0954d8171d6640a575
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-03-21 16:13:23 -0700

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,16 @@
+// Stings are pretty simple in Python, just use the double quotes or two single quotes
+>>> "Python is pretty darn easy"
+'Python is pretty darn easy'
+>>> 'Look on the bright side'
+'Look on the bright side'
+
+// One issue is that if you want to add quotes to a string you have to do this
+>>> 'Hassan\'s dog is super floofy'
+'Hassan's dog is super floofy
+
+// \n returns a new line
+// or you could just use a hard return (enter key)
+>>> "Hey dude
+this is pretty rad"
+' Hey dude
+this is pretty rad'
