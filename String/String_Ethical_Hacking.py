ea4830cbe309876ad0bcee7be7be4b6483ae0775
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2021-06-19 11:24:28 -0700

    Create String_Ethical_Hacking.py

---
+++ String/String_Ethical_Hacking.py
@@ -0,0 +1,14 @@
+% python
+>>> string1 = 'Wakanda'
+>>> string2 = 'Forever'
+>>> string1 + string2
+'WakandaForever'
+
+>>> string1 + " Pride " + string2
+' Wakanda Pride Forever '
+
+>>> string3 = string1 + string2 + "Strength"
+>>> string3
+'WakandaForeverStrength'
+
+>>> string3[2:10] #string 3 from index 2 (0-based) to 10
